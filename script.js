a257e55c34b9ec37c716a42bf770cd05d1297920
IDEV_PROJECT <47395735+gemer21@users.noreply.github.com>
2024-12-17 23:52:41 +0800

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 document.getElementById('generateBtn').addEventListener('click', async function () {
     const userName = document.getElementById('userName').value;
-    //const schoolName = document.getElementById('schoolName').value;
+    const certificateType = document.getElementById('certificateType').value;
     const loadingDots = document.getElementById('loadingDots');
     const pdfCanvas = document.getElementById('pdfCanvas');
+    const downloadBtn = document.getElementById('downloadBtn');
 
-    if (!userName) { // || !schoolName) {
-        alert("Please fill in both fields.");
+    if (!userName) {
+        showToast("Nama tidak boleh kosong.");
+        return;
+    }
+    
+    if (!certificateType) {
+    	showToast("Jenis sertifikat belum di pilih.");
         return;
     }
 
     loadingDots.style.display = 'block';
+    downloadBtn.style.display = 'none';
 
-    const url = 'SERTIFIKAT.pdf';
+    const url = certificateType;
     const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer());
     const pdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
     const page = pdfDoc.getPages()[0];
@@ -19,52 +26,39 @@ document.getElementById('generateBtn').addEventListener('click', async function 
     const fontSize = 24;
 
     const pageWidth = page.getWidth();
-
     const userNameWidth = font.widthOfTextAtSize(userName, fontSize);
-    //const schoolNameWidth = font.widthOfTextAtSize(schoolName, fontSize);
-
     const userNameX = (pageWidth - userNameWidth) / 2;
-    //const schoolNameX = (pageWidth - schoolNameWidth) / 2;
 
     page.drawText(userName, {
         x: userNameX,
-        y: 330, //340,
+        y: 330,
         font,
         size: fontSize,
     });
 
-    /*page.drawText(schoolName, {
-        x: schoolNameX,
-        y: 310,
-        font,
-        size: fontSize,
-    });*/
-
     const pdfBytes = await pdfDoc.save();
     displayPDFPreview(pdfBytes);
 
-    const downloadBtn = document.getElementById('downloadBtn');
-    downloadBtn.style.display = 'inline-block';
     downloadBtn.onclick = function () {
         const link = document.createElement('a');
         link.href = URL.createObjectURL(new Blob([pdfBytes], { type: 'application/pdf' }));
         link.download = 'certificate.pdf';
         link.click();
     };
-    loadingDots.style.display = 'none';
 });
 
 function displayPDFPreview(pdfBytes) {
     const fileURL = URL.createObjectURL(new Blob([pdfBytes], { type: 'application/pdf' }));
     const loadingDots = document.getElementById('loadingDots');
     const pdfCanvas = document.getElementById('pdfCanvas');
-    
+    const downloadBtn = document.getElementById('downloadBtn');
+
     const loadingTask = pdfjsLib.getDocument(fileURL);
     loadingDots.style.display = 'block';
-    
-    loadingTask.promise.then(function(pdf) {
-        pdf.getPage(1).then(function(page) {
-            const canvasWidth = 350;
+
+    loadingTask.promise.then(function (pdf) {
+        pdf.getPage(1).then(function (page) {
+            const canvasWidth = 285;
             const canvasHeight = 300;
             const viewport = page.getViewport({ scale: 1 });
             const scaleX = canvasWidth / viewport.width;
@@ -76,12 +70,25 @@ function displayPDFPreview(pdfBytes) {
             const context = pdfCanvas.getContext('2d');
             const renderContext = {
                 canvasContext: context,
-                viewport: scaledViewport
-            };            
-            page.render(renderContext).promise.then(function() {
+                viewport: scaledViewport,
+            };
+            page.render(renderContext).promise.then(function () {
                 loadingDots.style.display = 'none';
                 document.getElementById('pdfPreview').style.display = 'block';
+                downloadBtn.style.display = 'inline-block';
             });
         });
     });
+}
+
+function showToast(message) {
+    const toast = document.createElement('div');
+    toast.className = 'toast alert alert-success';
+    toast.innerHTML = message;
+    const container = document.getElementById('toast-container');
+    container.appendChild(toast);
+    toast.style.display = 'block';
+    setTimeout(() => {
+        toast.remove();
+    }, 1000);
 }
